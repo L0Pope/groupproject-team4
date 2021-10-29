f5e490edee77a0a031550cedec20f17394fd2f1a
TaRhondaMayberry <92880394+TaRhondaMayberry@users.noreply.github.com>
2021-10-29 10:34:05 -0700

    This entity was created to Update bullets as they move

---
+++ src/Entity.java
@@ -0,0 +1,20 @@
+
+public abstract class Entity {
+
+		
+		boolean isdead;
+		double x;
+		double y;
+		//add hitbox
+
+		public void update() {
+			
+		}
+			
+		public boolean isdead() {
+			return isdead;
+		}
+
+		
+
+}
---
+++ src/bullets.java
@@ -0,0 +1,34 @@
+import acm.graphics.*;
+import acm.graphics.GImage;
+import acm.graphics.GOval;
+
+public class bullets extends Entity{
+	double dy; //velocity
+	int damage;
+	GImage sprite;
+	GOval tempSprite;
+
+
+	bullets(double x, double y, double dy, int damage){
+		this.x = x;
+		this.y = y;
+		this.dy = dy;
+		this.damage = damage;
+		isdead = false;
+		
+		tempSprite = new GOval(x,y,50,50);
+
+	}
+		
+	public void update() {
+		//update position
+		
+		y += dy;
+		tempSprite.move(0, dy);
+		
+		//check collision with hitbox
+		
+		
+	}
+
+}
