8dda4b0214adca82926b4ffbf15254ec2dc909b0
wnavarro <wnavarro@STK-ECS-JR41343.stk.pacific.edu>
2021-11-17 10:07:55 -0800

    Music can play and stop on command. JukeBox implemented.

---
+++ src/JukeBox.java
@@ -0,0 +1,34 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+//lets get jazzy
+
+public class JukeBox {
+
+	static Clip Audio;
+	
+	JukeBox() {}
+	
+	static void PLAY (File SelectedAudio) {
+		try {
+			Audio = AudioSystem.getClip();
+			Audio.open(AudioSystem.getAudioInputStream(SelectedAudio));
+			Audio.start();
+			
+			Thread.sleep(Audio.getMicrosecondLength()/1000);
+		}
+		catch (Exception e) {}
+	}
+	
+	static void STOP () {
+		Audio.stop();
+	}
+	
+	public static void main (String [] args) {
+		File TestAudio = new File("TestAudio.wav");
+		PLAY(TestAudio);
+	}
+	
+}
