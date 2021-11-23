9b97dd302560c506b6659d1e86ab2ab01b5af35b
TaRhondaMayberry <92880394+TaRhondaMayberry@users.noreply.github.com>
2021-11-23 15:29:43 -0800

    Update Bullet.java
    add and remove bullet updates

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,24 +1,41 @@
 import acm.graphics.*;
 import acm.graphics.GImage;
 import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
 public class Bullet extends Entity{
+	private GraphicsProgram screen;
+	private static final int SIZE = 5;
 	double dy; //velocity
 	int damage;
 	GImage sprite;
 	GOval tempSprite;
 
 
-	public Bullet(double x, double y, double dy, int damage){
+	public Bullet(double x, double y, double dy, int damage, GraphicsProgram screen){
 		this.x = x;
 		this.y = y;
 		this.dy = dy;
 		this.damage = damage;
 		isdead = false;
+		this.screen = screen;
+		
 		
 		tempSprite = new GOval(x,y,50,50);
+		screen.add(tempSprite);
 
 	}
+	
+
+	
+//	public void add(PlayerShip playerShip) {
+//		playerShip.add(tempSprite);
+//	}
+	
+	public void removeBullet() {
+		screen.remove(tempSprite);
+	}
+	
 		
 	public void update() {
 		//updates position
@@ -28,6 +45,17 @@ public class Bullet extends Entity{
 		
 		//check collision with hitbox
 	}
+	
+	
+	public double getX() {
+		return x;
+	}
+	
+	
+	public double getY() {
+		return y;
+	}
+
 	
 
 	public String toString() {
