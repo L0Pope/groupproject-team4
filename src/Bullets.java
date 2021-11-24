25781491a24b34d7313311175a07ce58b852caf0
TaRhondaMayberry <92880394+TaRhondaMayberry@users.noreply.github.com>
2021-11-23 19:05:24 -0800

    Add files via upload
    Adding bullets to screen and add/remove bullets detail

---
+++ src/Bullets.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Bullets {
+private ArrayList<Bullet>bullets;
+	
+
+	public Bullets() {
+		bullets = new ArrayList<>();
+	}
+	
+		
+	public void update(){
+		
+		for(Bullet b : bullets) {
+			b.update();
+		}
+		bullets.removeIf(b ->(b.isdead()));
+	}
+
+	
+	
+	public void addBullet(Bullet b, PlayerShip playerShip) {
+		bullets.add(b);
+	//	b.add(playerShip);
+	}
+	
+	
+	//clearBullets will clear the entire bullet list
+	public void clearBullet() {
+		bullets.clear();
+	}
+		
+	
+	//check if each bullet falls within bounds.  If yes continue, if no remove from list and from canvas
+	public void removeBulletsOutOfBounds(int minX, int maxX, int minY, int maxY) {
+	
+		for (int i = bullets.size()-1; i>=0; i++) {
+			double x = bullets.get(i).getX();
+			double y = bullets.get(i).getY();
+			if(x < minX || x > maxX || y < minY || y > maxY) {
+				
+				
+				//remove from canvas
+				bullets.get(i).removeBullet();
+				
+				//remove from list
+				bullets.remove(i);
+				
+			}
+		
+		}
+		
+	}
+	
+	
+
+
+}
