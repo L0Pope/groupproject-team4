eaa71bca56fb765c3e3b2751f82e2923711aa3ed
William <w_navarro@u.pacific.edu>
2021-12-01 21:29:21 -0800

    Added Exit button to high score menu; Menus now uniform.

--- src/HighScore.java
+++ src/HighScore.java
@@ -1,47 +1,48 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import acm.graphics.GObject;
+import acm.graphics.GRect;
 
 public class HighScore extends GraphicsPane {
-	
-	// you will use program to get access to all of the GraphicsProgram calls
-	private MainApplication program; 			
-	private GParagraph para;
-	
 
-	public HighScore(MainApplication app) {
-		super();
-		program = app;
-		para = new GParagraph("Escape", 0, 590);
-		para.setFont("Comic Sans MS-24");
-	}
+    // you will use program to get access to all of the GraphicsProgram calls
+    private MainApplication program;
+    private GParagraph para;
+    private GButton box1;
 
-	@Override
-	public void showContents() {
-		program.add(para);
-		
-	}
 
-	@Override
-	public void hideContents() {
-		program.remove(para);
-	}
+    public HighScore(MainApplication app) {
+        super();
+        program = app;
+        box1 = new GButton("Escape", 4, 549, 300/3, 200/5);
+    }
 
-	/*
-	@Override
-	public void mousePressed(MouseEvent e) {
-		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
-			program.switchToMenu();
-		}
-	}
-}
-*/
-	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == 27) {
-			program.switchToMenu();
-		}
-	}
+    @Override
+    public void showContents() {
+        program.add(box1);
+
+    }
+
+    @Override
+    public void hideContents() {
+        program.removeAll();
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        GObject obj = program.getElementAt(e.getX(), e.getY());
+        if (obj == box1) {
+            program.switchToMenu();
+        }
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == 27) {
+            program.switchToMenu();
+        }
+    }
 }
