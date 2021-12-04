ab6bc824fb14610502f4d8f4be01d19ab2bd9e01
Felipe Martinez <f_martinez8@u.pacific.edu>
2021-12-03 19:24:44 -0800

    updated code

--- src/Game.java
+++ src/Game.java
@@ -18,148 +18,159 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class Game extends GraphicsPane implements ActionListener{
-	
-	File Game = new File("assets/bgm/fight!.wav");
-	private GImage backround;
-	private MainApplication program;
-	private SettingPane setting;
-	private ArrayList <enemyship> enemies = new ArrayList<enemyship>();
-	public static final int WINDOW_HEIGHT = 600;
-	public static final int WINDOW_WIDTH = 800;
-	public static final int SIZE = 25;
-	private Timer timer;
+    
+    File Game = new File("assets/bgm/fight!.wav");
+    private GImage backround;
+    private MainApplication program;
+    private SettingPane setting;
+    private ArrayList <enemyship> enemies = new ArrayList<enemyship>();
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int WINDOW_WIDTH = 800;
+    public static final int SIZE = 25;
+    private Timer timer;
 
-	//private static final int SPEED = 5;
-	
-	enemyship enemyShip;
-	enemyship bossShip;
-	PlayerShip playerShip;
-	//Hitbox hitbox;
-	
-	Game(MainApplication program){
-		backround = new GImage("assets/sprites/animatedMenu.gif");
-		this.program = program;
-		//hitbox = new Hitbox();
-		timer = new Timer(50, this);
-		//hitbox.run();
-		//run();
-	}	
-	
-	public void init() {
-		program.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	}
-	
-	public void run() {
-		program.add(backround);
-		addEnemies();
-		moveEnemies();
-		//addBoss();
-		//moveBoss();
-		makePlayerShip();
-		//playerShip.addKeyListeners();
-		timer.start();
-		checkCollision(playerShip);
-		
-	}
-	
-	private void checkCollision(PlayerShip playerShip) {
-		//for(enemyship e:enemies) {
-		//	for(int i = 0; i < e.bullets.bullets.size(); i++)  {
-		//		if(getElementAt( e.bullets.bullets[i] = )
-		//	}
-		//}
-	}
-	//Function Adds Two Rows of Enemies to An arraylist
-	private void addEnemies() {
-		
-		for(int i = SIZE; i < 750/*WINDOW_WIDTH-SIZE/2*/; i += 50) {
-			enemyShip = new enemyship(shipType.ENEMYSHIP, i, 25, program);
-			enemies.add(enemyShip);
-		}
-		
-		for(int i = SIZE+100; i < 650; i+=50) {
-			enemyShip = new enemyship(shipType.ENEMYSHIP, i, 75, program);
-			enemies.add(enemyShip);
-		}
-	}
-	//Function Looks into the Arraylist and places the enemies onto the screen
-	private void moveEnemies() {
-		for(enemyship e:enemies) {
-			e.makeEnemy();
-		}
-	}
-	private void addBoss() {
-		bossShip = new enemyship(shipType.BOSSSHIP, 20, 50, program);
-	}
-	
-	private void moveBoss() {
-		bossShip.makeBoss();
-	}
-	
+    //private static final int SPEED = 5;
+    
+    enemyship enemyShip;
+    enemyship bossShip;
+    PlayerShip playerShip;
+    //Hitbox hitbox;
+    
+    Game(MainApplication program){
+        backround = new GImage("assets/sprites/animatedMenu.gif");
+        this.program = program;
+        //hitbox = new Hitbox();
+        timer = new Timer(50, this);
+        //hitbox.run();
+        //run();
+    }    
+    
+    public void init() {
+        program.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+    
+    public void run() {
+        program.add(backround);
+        addEnemies();
+        moveEnemies();
+        //addBoss();
+        //moveBoss();
+        makePlayerShip();
+        //playerShip.addKeyListeners();
+        timer.start();
+        //updateAllBullets();
+        
+    }
+    
+    private void updateAllBullets() {
+            for(enemyship e : enemies) {
+                    for(Bullet b : e.bullets.bullets) {
+                        b.update();
+                        if(b.returnBulletX() < playerShip.returnPlayerX()) {
+                            System.out.println("HIT!");
+                        }
+                        System.out.println(b.returnBulletX());
+                        System.out.println(playerShip.returnPlayerX());
+                        //System.out.println("test");
+                    }        
+            }
+            //System.out.println("test");
 
-	@Override
-	public void showContents() {
-		run();
-	}
-	
-	@Override
-	public void hideContents() {
-		for(int i = 0; i<enemies.size(); i++) {
+    }
+    //Function Adds Two Rows of Enemies to An arraylist
+    private void addEnemies() {
+        
+        for(int i = SIZE; i < 750/*WINDOW_WIDTH-SIZE/2*/; i += 50) {
+            enemyShip = new enemyship(shipType.ENEMYSHIP, i, 25, program);
+            enemies.add(enemyShip);
+        }
+        
+        for(int i = SIZE+100; i < 650; i+=50) {
+            enemyShip = new enemyship(shipType.ENEMYSHIP, i, 75, program);
+            enemies.add(enemyShip);
+        }
+    }
+    //Function Looks into the Arraylist and places the enemies onto the screen
+    private void moveEnemies() {
+        for(enemyship e:enemies) {
+            e.makeEnemy();
+        }
+    }
+    private void addBoss() {
+        bossShip = new enemyship(shipType.BOSSSHIP, 20, 50, program);
+    }
+    
+    private void moveBoss() {
+        bossShip.makeBoss();
+    }
+    
+
+    @Override
+    public void showContents() {
+        run();
+    }
+    
+    @Override
+    public void hideContents() {
+    	for(int i = 0; i<enemies.size(); i++) {
 			enemies.get(i).finalize();
 		}
-		program.removeAll();
-	}
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-		
-	}
+        program.removeAll();
+    }
+    
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+    
+    
+    
+
+    //Calls the PlayerShip class to create and add playerShip onto the screen
+    private void makePlayerShip() {
+        playerShip = new PlayerShip(program);
+        playerShip.makePlayerShip();
+        //addKeyListeners();
+    }
+    
+    //KeyListeners used to move playerShip using WASD
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        int forward = program.getForward();
+        int left = program.getLeft();
+        int down = program.getDown();
+        int right = program.getRight();
+        if (key == forward) {
+            playerShip.move(1);
+        } else if(key == left) {
+            playerShip.move(2);
+        } else if(key == down) {
+            playerShip.move(3);
+        } else if (key == right) {
+            playerShip.move(4);
+        } else if (key == KeyEvent.VK_SPACE) {
+            playerShip.move(5);
+        }
+        if(key == KeyEvent.VK_ESCAPE) {
+            JukeBox.STOP();
+            program.switchToMenu();
+        }
+    }
+    
+    //Test Test
+    public static void main(String[] args) {
+        //new Game().start();
+    }
 
 
-	//Calls the PlayerShip class to create and add playerShip onto the screen
-	private void makePlayerShip() {
-		playerShip = new PlayerShip(program);
-		playerShip.makePlayerShip();
-		//addKeyListeners();
-	}
-	
-	//KeyListeners used to move playerShip using WASD
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int key = e.getKeyCode();
-		int forward = program.getForward();
-		int left = program.getLeft();
-		int down = program.getDown();
-		int right = program.getRight();
-		if (key == forward) {
-			playerShip.move(1);
-		} else if(key == left) {
-			playerShip.move(2);
-		} else if(key == down) {
-			playerShip.move(3);
-		} else if (key == right) {
-			playerShip.move(4);
-		} else if (key == KeyEvent.VK_SPACE) {
-			playerShip.move(5);
-		}
-		if(key == KeyEvent.VK_ESCAPE) {
-			JukeBox.STOP();
-			program.switchToMenu();
-		}
-	}
-	
-	//Test Test
-	public static void main(String[] args) {
-		//new Game().start();
-	}
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		playerShip.update();
-		//hitbox.checkCollision();
-		// update all enemies once here
-		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        playerShip.update();
+        updateAllBullets();
+        //hitbox.checkCollision();
+        // update all enemies once here
+        
+    }
 }
--- src/enemyship.java
+++ src/enemyship.java
@@ -16,164 +16,153 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class enemyship extends GraphicsProgram implements ActionListener{
-	private Timer movement;
-	private RandomGenerator rgen;
-	private GraphicsProgram screen;
-	private shipType typeShip;
-	private healthSystem bossHealth;
-	
-	public static final int SIZE = 25;
-	public static final int SPEED = 2;
-	public static final int MS = 50;
-	public static final int WINDOW_HEIGHT = 600;
-	public static final int WINDOW_WIDTH = 800;
-	private int numTimes = -1;
-	public Boolean right = true;
-	private int x, y;	
-	//private GRect enemy;
-	private GImage enemy;
-	public Bullets bullets;
-	private MainApplication program;
-	private boolean friendlyBullet = false;
-	
-	//Each EnemyShip has its own coordinates, use x and y to locate it 
-	public enemyship(shipType typeShip, int x, int y, GraphicsProgram screen) { //location of the ship
-		this.typeShip = typeShip;
-		this.x=x;
-		this.y=y;
-		this.screen = screen;
-		bullets = new Bullets();
-		rgen = RandomGenerator.getInstance();
-	
-	}	
-	
-	public void update() {
+    private Timer movement;
+    private RandomGenerator rgen;
+    private GraphicsProgram screen;
+    private shipType typeShip;
+    private healthSystem bossHealth;
+    
+    public static final int SIZE = 25;
+    public static final int SPEED = 2;
+    public static final int MS = 50;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int WINDOW_WIDTH = 800;
+    private int numTimes = -1;
+    public Boolean right = true;
+    private int x, y;    
+    //private GRect enemy;
+    private GImage enemy;
+    public Bullets bullets;
+    private MainApplication program;
+    private boolean friendlyBullet = false;
+    
+    //Each EnemyShip has its own coordinates, use x and y to locate it 
+    public enemyship(shipType typeShip, int x, int y, GraphicsProgram screen) { //location of the ship
+        this.typeShip = typeShip;
+        this.x=x;
+        this.y=y;
+        this.screen = screen;
+        bullets = new Bullets();
+        rgen = RandomGenerator.getInstance();
+    
+    }    
+    
+    public void update() {
 
-		updateBullet();
-		int rand = rgen.nextInt(1000);
-		if(typeShip == shipType.BOSSSHIP) {
-			if(rand < 5) {
-				fireBullet();
-			}
-		}else if(typeShip == shipType.ENEMYSHIP) {
-			if(rand <3)
-				fireBullet();
-		}
-		}
-	public void updateBullet() {
-		for(Bullet b : bullets.bullets) {
-			b.update();
-		}
-	}
-//		removeBulletsOutOfBounds();
-	
-	protected void finalize() {
+        updateBullet();
+        int rand = rgen.nextInt(1000);
+        if(typeShip == shipType.BOSSSHIP) {
+            if(rand < 5) {
+                fireBullet();
+            }
+        }else if(typeShip == shipType.ENEMYSHIP) {
+            if(rand <3)
+                fireBullet();
+        }
+        }
+    public void updateBullet() {
+        for(Bullet b : bullets.bullets) {
+            b.update();
+        }
+    }
+    
+    protected void finalize() {
 		movement.stop();
 		bullets.clearBullet();
 	}
-	
-	
-	public void fireBullet(){
-		bullets.addBullet(new Bullet(x,y, 5, 1, friendlyBullet, screen));
-	}
-	
-	public void run() {
-		//rgen = RandomGenerator.getInstance();
-		//movement = new Timer(MS, this);
-		makeBoss();
-		//movement.start();
-		//addMouseListeners();
-	}
-	
-	//Makes Single Grect that also has a timer associated with it
-	public void makeEnemy() {
-		enemy = new GImage("assets/sprites/badguy.gif", x, y);
-		//enemy = new GRect(x-SIZE/2, y, SIZE, SIZE);
-		//enemy.setColor(new Color(0,255, 0));
-		//enemy.setFilled(true);
-		screen.add(enemy);
-		movement = new Timer(MS, this);
-		movement.start();
-	}
-	
-	public void makeBoss() {
-		//This is just a temp for the boss
-		enemy = new GImage("assets/sprites/badguy.gif");
-		//enemy = new GRect(x-SIZE/2, y, SIZE*4, SIZE*4);
-		//enemy.setColor(new Color(0,255,0));
-		//enemy.setFilled(true);
-		//add(enemy);
-		bossHealth = new healthSystem(shipType.BOSSSHIP, 3, false);
-		screen.add(enemy); // enable this for game.java
-		movement = new Timer(MS, this);
-		movement.start();
-	}
-	
-	//Calls to the Function that will move the enemies left and right
-	public void actionPerformed(ActionEvent e) {
-		numTimes ++;
-		moveEnemy();
-		// remove update from here
-		update();
-	}
-	
-	//Utilizes the timer so that it will move left and right everytime the timer reaches 5
-	public void moveEnemy() {
-		if(typeShip == shipType.ENEMYSHIP) {
-			if(numTimes == 5) {
-				if(right) {
-					right = false;
-					numTimes =0;
-				}
-				else if(!right) {
-					right = true;
-					numTimes = 0;
-				}
-			}
-			if(right) {
-				x += 2;
-				enemy.move(2, 0);	//may have to change to check setlocation 	
-			}
-			if(!right) {
-				x +=-2;
-				enemy.move(-2, 0);
-			}
-		}
-		else {
-			if(numTimes == 130) { // if x is at the end of screen switch
-				if(right) {
-					right = false;
-					numTimes = 0;
-				}
-				else if(!right) {
-					right = true;
-					numTimes = 0;
-				}
-			}
-			if(right) {
-				x +=5;
-				enemy.move(5, 0);
-			}
-			if(!right) {
-				x +=-5;
-				enemy.move(-5, 0);
-			}
-		}
-	}
-	
-	//Call this to Delete the Gobject
-	/*private void killEnemy() {
-		System.out.println("Dead!");
-		remove(enemy);
-	}
-	*/
-	public void init() {
-		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	}
-	
-	public static void main(String args[]) {
-		//new enemyship(shipType.BOSSSHIP, 20,50).start();
-	}
+    
+    public void fireBullet(){
+        bullets.addBullet(new Bullet(x,y, 5, 1, friendlyBullet, screen));
+    }
+    
+    public void run() {
+        //rgen = RandomGenerator.getInstance();
+        //movement = new Timer(MS, this);
+        makeBoss();
+        //movement.start();
+        //addMouseListeners();
+    }
+    
+    //Makes Single Grect that also has a timer associated with it
+    public void makeEnemy() {
+        enemy = new GImage("assets/sprites/badguy.gif", x, y);
+        screen.add(enemy);
+        movement = new Timer(MS, this);
+        movement.start();
+    }
+    
+    public void makeBoss() {
+        //This is just a temp for the boss
+        enemy = new GImage("assets/sprites/badguy.gif");
+        bossHealth = new healthSystem(shipType.BOSSSHIP, 3, false);
+        screen.add(enemy); // enable this for game.java
+        movement = new Timer(MS, this);
+        movement.start();
+    }
+    
+    //Calls to the Function that will move the enemies left and right
+    public void actionPerformed(ActionEvent e) {
+        numTimes ++;
+        moveEnemy();
+        // remove update from here
+        update();
+    }
+    
+    //Utilizes the timer so that it will move left and right everytime the timer reaches 5
+    public void moveEnemy() {
+        if(typeShip == shipType.ENEMYSHIP) {
+            if(numTimes == 5) {
+                if(right) {
+                    right = false;
+                    numTimes =0;
+                }
+                else if(!right) {
+                    right = true;
+                    numTimes = 0;
+                }
+            }
+            if(right) {
+                x += 2;
+                enemy.move(2, 0);    //may have to change to check setlocation     
+            }
+            if(!right) {
+                x +=-2;
+                enemy.move(-2, 0);
+            }
+        }
+        else {
+            if(numTimes == 130) { // if x is at the end of screen switch
+                if(right) {
+                    right = false;
+                    numTimes = 0;
+                }
+                else if(!right) {
+                    right = true;
+                    numTimes = 0;
+                }
+            }
+            if(right) {
+                x +=5;
+                enemy.move(5, 0);
+            }
+            if(!right) {
+                x +=-5;
+                enemy.move(-5, 0);
+            }
+        }
+    }
+    
+    //Call this to Delete the Gobject
+    /*private void killEnemy() {
+        System.out.println("Dead!");
+        remove(enemy);
+    }
+    */
+    public void init() {
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+    
+    public static void main(String args[]) {
+        //new enemyship(shipType.BOSSSHIP, 20,50).start();
+    }
 }
-//add kill enemy function
-//change arraylist to single and make arraylist in main
