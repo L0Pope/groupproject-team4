163056321a6eebdd06f27e7d8ea87a44fcebc9e9
Felipe Martinez <f_martinez8@u.pacific.edu>
2021-12-04 18:59:27 -0800

    boss now moves to end of screen

--- src/enemyship.java
+++ src/enemyship.java
@@ -140,7 +140,7 @@ public class enemyship extends GraphicsProgram implements ActionListener{
             }
         }
         else {
-            if(numTimes == 130) { // if x is at the end of screen switch
+            if(numTimes == 150) { // if x is at the end of screen switch
                 if(right) {
                     right = false;
                     numTimes = 0;
